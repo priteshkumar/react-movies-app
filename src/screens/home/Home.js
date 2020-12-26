8a4194e9a92d4b19d402ff1dd6f21aa1a58ba563
Pritesh Kumar <pritesh.kumar2@gmail.com>
2020-12-26 11:18:25 +0530

    added Home component

---
+++ src/screens/home/Home.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './Home.css';
+
+class Homme extends Component {
+    render(){
+        return (
+            <div></div>
+        );
+    }
+}
+
+export default Homme;
