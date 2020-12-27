017d1365cfc6a8992147429351f52bce1f0f2ef2
Pritesh Kumar <pritesh.kumar2@gmail.com>
2020-12-27 20:42:40 +0530

    add tabs

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -3,11 +3,14 @@ import './Header.css';
 import Button from '@material-ui/core/Button';
 import logo from '../../assets/logo.svg';
 import Modal from 'react-modal';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+
 
 class Header extends Component {
     constructor(props) {
         super(props);
-        this.state = { ModalIsOpen: false };
+        this.state = { ModalIsOpen: false, value: 0 };
     }
 
     openModalHandler = () => {
@@ -15,7 +18,11 @@ class Header extends Component {
     }
 
     closeModalHandler = () => {
-        this.setState({ModalIsOpen:false});
+        this.setState({ ModalIsOpen: false });
+    }
+    
+    onTabchangeHandler = (event, value) => {
+        this.setState({ value });
     }
 
     render() {
@@ -24,6 +31,10 @@ class Header extends Component {
                 <img src={logo} className="app-logo" alt="logo" />
                 <Button variant="contained" color="default" size="large" onClick={this.openModalHandler}>Login</Button>
                 <Modal ariaHideApp={false} isOpen={this.state.ModalIsOpen} contentLabel="Login" onRequestClose={this.closeModalHandler}>
+                    <Tabs value={this.state.value} onChange={this.onTabchangeHandler}>
+                        <Tab label="LOGIN" />
+                        <Tab label="REGISTER" />
+                    </Tabs>
                 </Modal>
             </div>
         );
