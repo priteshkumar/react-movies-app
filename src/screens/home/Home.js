3f1dcfecd8232e53e8455a7ed5fd4d5e7054406a
Pritesh Kumar <pritesh.kumar2@gmail.com>
2021-01-02 20:17:02 +0530

    add release date filter

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -12,6 +12,13 @@ import FormControl from "@material-ui/core/FormControl";
 import { Typography } from "@material-ui/core";
 import InputLabel from "@material-ui/core/InputLabel";
 import Input from "@material-ui/core/Input";
+import Select from "@material-ui/core/Select";
+import MenuItem from "@material-ui/core/MenuItem";
+import Checkbox from "@material-ui/core/Checkbox";
+import ListItemText from "@material-ui/core/ListItemText";
+import TextField from "@material-ui/core/TextField";
+import genres from "../../common/genres";
+import artists from "../../common/artists";
 
 const styles = (theme) => ({
   root: {
@@ -52,12 +59,24 @@ const styles = (theme) => ({
 class Home extends Component {
   constructor(props) {
     super(props);
-    this.state = { movieName: "" };
+    this.state = {
+      movieName: "",
+      genres: [],
+      artists: [],
+    };
   }
 
   movieNameChangeHandler = (event) => {
     this.setState({ movieName: event.target.value });
-    console.log("logging moviename");
+    //console.log("logging moviename");
+  };
+
+  genreSelectHandler = (event) => {
+    this.setState({ genres: event.target.value });
+  };
+
+  artistSelectHandler = (event) => {
+    this.setState({ artists: event.target.value });
   };
 
   render() {
@@ -122,6 +141,77 @@ class Home extends Component {
                   <Input
                     id="movieName"
                     onChange={this.movieNameChangeHandler}
+                  />
+                </FormControl>
+                <FormControl className={classes.formControl}>
+                  <InputLabel htmlFor="select-multiple-checkbox">
+                    Genres
+                  </InputLabel>
+                  <Select
+                    multiple
+                    input={<Input id="select-multiple-checkbox-genre" />}
+                    renderValue={(selected) => selected.join(",")}
+                    value={this.state.genres}
+                    onChange={this.genreSelectHandler}
+                  >
+                    <MenuItem value="0">None</MenuItem>
+                    {genres.map((genre) => (
+                      <MenuItem key={genre.id} value={genre.name}>
+                        <Checkbox
+                          checked={this.state.genres.indexOf(genre.name) > -1}
+                        />
+                        <ListItemText primary={genre.name} />
+                      </MenuItem>
+                    ))}
+                  </Select>
+                </FormControl>
+                <FormControl className={classes.formControl}>
+                  <InputLabel htmlFor="select-multiple-checkbox">
+                    Artists
+                  </InputLabel>
+                  <Select
+                    multiple
+                    input={<Input id="select-multiple-checkbox" />}
+                    renderValue={(selected) => selected.join(",")}
+                    value={this.state.artists}
+                    onChange={this.artistSelectHandler}
+                  >
+                    <MenuItem value="0">None</MenuItem>
+                    {artists.map((artist) => (
+                      <MenuItem
+                        key={artist.id}
+                        value={artist.first_name + " " + artist.last_name}
+                      >
+                        <Checkbox
+                          checked={
+                            this.state.artists.indexOf(
+                              artist.first_name + " " + artist.last_name
+                            ) > -1
+                          }
+                        />
+                        <ListItemText
+                          primary={artist.first_name + " " + artist.last_name}
+                        />
+                      </MenuItem>
+                    ))}
+                  </Select>
+                </FormControl>
+                <FormControl className={classes.formControl}>
+                  <TextField
+                    id="releaseDateStart"
+                    label="Release Date Start"
+                    type="date"
+                    defaultValue=""
+                    InputLabelProps={{ shrink: true }}
+                  />
+                </FormControl>
+                <FormControl className={classes.formControl}>
+                  <TextField
+                    id="releaseDateEnd"
+                    label="Release Date End"
+                    type="date"
+                    defaultValue=""
+                    InputLabelProps={{ shrink: true }}
                   />
                 </FormControl>
               </CardContent>
